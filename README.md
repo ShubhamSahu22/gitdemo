9ba183406274eac6cbfe64451c0f255055ddb0d5
student apnaCollage <shubhamsahu410@gamil.com>
2024-09-10 12:21:40 +0530

    discruption of circle

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gitdemo
 git demo python program
+ARea of circle 
+
---
+++ main.py
@@ -0,0 +1,12 @@
+#area of circle
+def area(radius):
+    return 3.14 * radius * radius
+print(area(5))
+#circumference of circle
+def circumference(radius):
+    return 2 * 3.14 * radius
+print(circumference(5))
+#area of square
+def area(side):
+    return side * side
+print(area(5))
